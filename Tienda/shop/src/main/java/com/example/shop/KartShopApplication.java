f1b043a66a9fbde330b6e61a8766b103b5babc25
TacoConLimon <juandavidgove24@gmail.com>
2024-04-05 16:45:47 -0500

    primera conexion de tablas

--- Tienda/shop/src/main/java/com/example/shop/KartShopApplication.java
+++ Tienda/shop/src/main/java/com/example/shop/KartShopApplication.java
@@ -3,8 +3,8 @@ package com.example.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class KartShopApplication {
+@SpringBootApplication ()
+public class KartShopApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(KartShopApplication.class, args);
